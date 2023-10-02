87fccbe649cad40e087d4763b33523aadd5ea819
19857191790 <>
2023-10-02 10:12:07 +0800

    test01

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import requireTransform from 'vite-plugin-require-transform';
+import nodeResolve from '@rollup/plugin-node-resolve'
 export default defineConfig({
   plugins: [
     vue(),
     requireTransform({
       fileRegex: /.js$|.vue$|.png$|.ts$|.jpg$/
+    }),
+    nodeResolve({
+      preferBuiltins: true
     })
   ],
   base:'./',
