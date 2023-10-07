69b4582746edf06b6e3791cfb03fe921c52c4ba9
19857191790 <>
2023-10-07 09:47:31 +0800

    回退

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,56 +1,44 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import layout from "@/layout/index.vue"
-import AppMain from "@/layout/AppMain/AppMain.vue"
+ 
 const routes = [
     {
       path: '/',
-      redirect: '/main',
+      redirect: '/home',
       component: layout,
       children:[
+        { path: '/home', component: () => import('@/views/Home/Home.vue'),hidden:true },
+        { path: '/about', component: () => import('@/views/About/About.vue'),hidden:true },
         {
-          path: '/main',
-          component: AppMain,
-          redirect: '/main/home',
-          hidden:true,
-          children:[
-            {
-              path:'home',
-              hidden:true,
-              component: ()=>import('@/views/Home/Home.vue'),
-            },
-            {
-              path:'sassStarSky',
-              name:'Sass星空',
-              component: ()=>import('@/views/components/sassStarSky.vue'),
-            },
-            {
-              path:'particles',
-              name:'Particles',
-              component: ()=>import('@/views/components/ParticlesView/particles.vue'),
-            },
-            {
-              path:'upload',
-              name:'七牛云文件上传',
-              component: ()=>import('@/views/components/uploadView/uploadView.vue'),
-            },
-            {
-              path:'svgAnimation',
-              name:'图标动画',
-              component: ()=>import('@/views/components/svgAnimation.vue'),
-            },
-            {
-              path:'gridAnimation',
-              name:'宫格hover动画',
-              component: ()=>import('@/views/components/gridAnimation.vue'),
-            }
-          ]
+          path:'sassStarSky',
+          name:'Sass星空',
+          component: ()=>import('@/views/components/sassStarSky.vue'),
         },
-        { path: '/about', component: () => import('@/views/About/About.vue'),hidden:true }
+        {
+          path:'particles',
+          name:'Particles',
+          component: ()=>import('@/views/components/ParticlesView/particles.vue'),
+        },
+        {
+          path:'upload',
+          name:'七牛云文件上传',
+          component: ()=>import('@/views/components/uploadView/uploadView.vue'),
+        },
+        {
+          path:'svgAnimation',
+          name:'图标动画',
+          component: ()=>import('@/views/components/svgAnimation.vue'),
+        },
+        {
+          path:'gridAnimation',
+          name:'宫格hover动画',
+          component: ()=>import('@/views/components/gridAnimation.vue'),
+        }
       ]
     }
 ]
  
 export default createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
